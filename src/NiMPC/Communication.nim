805317782e90e2edca48518fcb40984a34f57955
Mark Spanbroek <mark@spanbroek.net>
2019-07-16 12:05:40 +0200

    Add party.receive()

--- src/NiMPC/Communication.nim
+++ src/NiMPC/Communication.nim
@@ -1,6 +1,17 @@
+import tables
+import hashes
 import bigints
 
 type Party* = ref object
 
-method send*(sender: Party, recipient: Party, value: BigInt) =
-  discard
+proc hash*(party: Party): Hash =
+  result = hash(unsafeAddr party[])
+
+var latestValues = newTable[Party, BigInt]()
+
+method send*(sender: Party, recipient: Party, value: BigInt) {.base.} =
+  latestValues[recipient] = value
+
+method receive*(recipient: Party, sender: Party): BigInt {.base.} =
+  if latestValues.hasKey(recipient):
+    result = latestValues[recipient]
--- tests/testCommunication.nim
+++ tests/testCommunication.nim
@@ -10,3 +10,17 @@ test "can send from one party to another":
   let party2 = Party()
   let value: BigInt = 42
   party1.send(party2, value)
+
+test "can receive values for other parties":
+  let party1 = Party()
+  let party2 = Party()
+  let value: BigInt = 42
+  party1.send(party2, value)
+  check party2.receive(party1) == value
+
+test "value is received by recipient only":
+  let party1 = Party()
+  let party2 = Party()
+  let value: BigInt = 42
+  party1.send(party2, value)
+  check party1.receive(party2) != value
