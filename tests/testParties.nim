868d90e163ffec9fb7c7c8e15c7e1356110a4b4c
Mark Spanbroek <mark@spanbroek.net>
2019-07-17 12:27:07 +0200

    simplify

--- tests/testParties.nim
+++ tests/testParties.nim
@@ -5,9 +5,7 @@ test "can create a party":
   check Party() != nil
 
 test "connects parties":
-  let party1 = Party()
-  let party2 = Party()
-  let party3 = Party()
+  let party1, party2, party3 = Party()
   connect(party1, party2, party3)
   check party1.peers == @[party2, party3]
   check party2.peers == @[party1, party3]
