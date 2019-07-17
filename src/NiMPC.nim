bc36f088cd5665314dc0f45b1040ad70731667d3
Mark Spanbroek <mark@spanbroek.net>
2019-07-17 13:14:02 +0200

    Remove boilerplate code

--- src/NiMPC.nim
+++ src/NiMPC.nim
@@ -2,6 +2,3 @@ include NiMPC/BigIntegers
 include NiMPC/Communication
 include NiMPC/SecretSharing
 include NiMPC/Math
-
-proc add*(x, y: int): int =
-  return x + y
--- tests/testNiMPC.nim
+++
@@ -1,5 +0,0 @@
-import unittest
-import NiMPC
-
-test "adds":
-    check add(2, 3) == 5
