04823c9ce8c3770852921a43e6809050f711447f
Mark Spanbroek <mark@spanbroek.net>
2019-07-17 12:04:44 +0200

    Rename: myShare -> share, and export it

--- src/NiMPC/SecretSharing.nim
+++ src/NiMPC/SecretSharing.nim
@@ -6,16 +6,16 @@ import BigIntegers
 import Communication
 
 type Secret* = object
-  myShare: BigInt
+  share*: BigInt
 
 proc random*: Secret =
-  result = Secret(myShare: getRandom())
+  result = Secret(share: getRandom())
 
 method reveal*(party: Party, secret: Secret, recipient: Party) {.async,base.} =
-  await party.send(recipient, secret.myShare)
+  await party.send(recipient, secret.share)
 
 method obtain*(party: Party, secret: Secret): Future[BigInt] {.async,base.} =
-  var shares = @[secret.myShare]
+  var shares = @[secret.share]
   for sender in party.peers:
     shares.add(await party.receive(sender))
   result = shares.foldl(a + b)
