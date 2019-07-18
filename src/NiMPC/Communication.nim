01826c4b94393a08835f662254ae5190d9b506bf
Mark Spanbroek <mark@spanbroek.net>
2019-07-18 09:58:30 +0200

    remove duplication

--- src/NiMPC/Communication.nim
+++ src/NiMPC/Communication.nim
@@ -13,8 +13,11 @@ var inboxes = Inboxes()
 proc inbox(party: Party): Inbox =
   inboxes.mgetOrPut(party, Inbox())
 
+proc messagesFrom(inbox: Inbox, party: Party): Messages =
+  inbox.mgetOrPut(party, newFutureStream[uint32]())
+
 method send*(sender: Party, recipient: Party, value: uint32): Future[void] {.async,base.} =
-  let messages = recipient.inbox.mgetOrPut(sender, newFutureStream[uint32]())
+  let messages = recipient.inbox.messagesFrom(sender)
   await messages.write(value)
 
 method broadcast*(sender: Party, value: uint32): Future[void] {.async,base.} =
@@ -22,6 +25,6 @@ method broadcast*(sender: Party, value: uint32): Future[void] {.async,base.} =
     await sender.send(recipient, value)
 
 method receive*(recipient: Party, sender: Party): Future[uint32] {.async,base.} =
-  let messages = recipient.inbox.mgetOrPut(sender, newFutureStream[uint32]())
+  let messages = recipient.inbox.messagesFrom(sender)
   let (_, received) = await messages.read()
   result = received
