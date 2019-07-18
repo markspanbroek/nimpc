a22ed1839ee3b50ac38dac57e93267ca9a82bdac
Mark Spanbroek <mark@spanbroek.net>
2019-07-18 10:56:29 +0200

    Simplify addition test using share()

--- tests/testMath.nim
+++ tests/testMath.nim
@@ -5,12 +5,14 @@ import NiMPC
 
 asynctest "adds secret numbers":
   twoParties:
-    let a1, b1 = random()
-    let c1 = a1 + b1
-    await party1.reveal(c1, party2)
+    let a = await party1.share(40)
+    let b = await party2.share(2)
 
-    let a2, b2 = random()
-    let c2 = a2 + b2
+    let c1 = a + await party1.obtain(party2)
+    let c2 = b + await party2.obtain(party1)
+
+    await party1.reveal(c1, party2)
     await party2.reveal(c2, party1)
 
-    check (await party1.open(c1)) == (await party2.open(c2))
+    check (await party1.open(c1)) == 42
+    check (await party2.open(c2)) == 42
