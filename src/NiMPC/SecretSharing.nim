7ca90fb2d05140e870e8ebc04f38b03c86c001a7
Mark Spanbroek <mark@spanbroek.net>
2019-07-18 10:40:48 +0200

    simplify

--- src/NiMPC/SecretSharing.nim
+++ src/NiMPC/SecretSharing.nim
@@ -12,19 +12,21 @@ proc random*: Secret =
 method reveal*(party: Party, secret: Secret, recipient: Party) {.async,base.} =
   await party.send(recipient, secret.share)
 
+proc open(shares: seq[uint32]): uint32 =
+  result = shares.foldl(a + b)
+
 method open*(party: Party, secret: Secret): Future[uint32] {.async,base.} =
   var shares = @[secret.share]
   for sender in party.peers:
     shares.add(await party.receive(sender))
-  result = shares.foldl(a + b)
+  result = open(shares)
 
 method share*(party: Party, input: uint32): Future[Secret] {.async,base.} =
-  let numberOfParties = party.peers.len + 1
-  let shares = newSeqWith(numberOfParties, getRandom())
-  let r = shares.foldl(a + b)
-  for i in 0..<party.peers.len:
-    await party.send(party.peers[i], shares[i])
-  result = Secret(share: shares[^1] - r + input)
+  var shares = @[getRandom()]
+  for receiver in party.peers:
+    shares.add getRandom()
+    await party.send(receiver, shares[^1])
+  result = Secret(share: shares[0] - open(shares) + input)
 
 method obtain*(party: Party, sender: Party): Future[Secret] {.async,base.} =
   result = Secret(share: await party.receive(sender))
