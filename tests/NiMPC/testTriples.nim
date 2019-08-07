f5842a50a5a293b246ae74fef231d1a6fdc3b8b3
Mark Spanbroek <mark@spanbroek.net>
2019-08-07 11:13:49 +0200

    Add test for two party triple generation

--- tests/NiMPC/testTriples.nim
+++ tests/NiMPC/testTriples.nim
@@ -2,11 +2,32 @@ import unittest
 import asynctest
 import parties
 import NiMPC/Parties
+import NiMPC/SecretSharing
 import NiMPC/Triples
 
 suite "multiplication triples":
 
-  asynctest "generates a dummy triple when there's only one party":
-    singleParty:
-      let triple = await party.triple()
-      check triple.a.share * triple.b.share == triple.c.share
+  suite "single party":
+
+    asynctest "generates a dummy triple":
+      singleParty:
+        let triple = await party.triple()
+        check triple.a.share * triple.b.share == triple.c.share
+
+  suite "two parties":
+
+    proc createAndOpenTriple(): Future[tuple[a, b, c: uint32]] {.async.} =
+      twoParties:
+        let (fut1, fut2) = (party1.triple(), party2.triple())
+        let (triple1, triple2) = (await fut1, await fut2)
+        await triple2.a.reveal()
+        await triple2.b.reveal()
+        await triple2.c.reveal()
+        result.a = await triple1.a.open()
+        result.b = await triple1.b.open()
+        result.c = await triple1.c.open()
+
+    asynctest "generates a correct triple":
+      twoParties:
+        let (a,b,c) = await createAndOpenTriple()
+        check a * b == c
