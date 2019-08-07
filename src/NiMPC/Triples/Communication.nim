0e138d49c872ce8be33c9628ea9610ff4195729e
Mark Spanbroek <mark@spanbroek.net>
2019-08-07 16:54:10 +0200

    Sending and receiving of shares

---
+++ src/NiMPC/Triples/Communication.nim
@@ -0,0 +1,12 @@
+import asyncdispatch
+import ../SecretSharing
+import ../Communication
+export Communication
+
+proc receiveShare*(receiver: Party, sender: Party):
+                    Future[Share] {.async.} =
+  result = await receive[Share](receiver, sender)
+
+proc receiveShares*(receiver: Party, sender: Party):
+                    Future[seq[Share]] {.async.} =
+  result = await receive[seq[Share]](receiver, sender)
---
+++ tests/NiMPC/testTriplesCommunication.nim
@@ -0,0 +1,20 @@
+import unittest
+import asynctest
+import parties
+import NiMPC/Random
+import NiMPC/Triples/Communication
+import NiMPC/SecretSharing
+
+suite "communication for triple generation":
+
+  asynctest "can send share":
+    twoParties:
+      let share = random[Share]()
+      await party1.send(party2, share)
+      check (await party2.receiveShare(party1)) == share
+
+  asynctest "can send a sequence of shares":
+    twoParties:
+      let shares = random[array[2, Share]]()
+      await party1.send(party2, shares)
+      check (await party2.receiveShares(party1)) == shares
