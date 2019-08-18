8c6fb6be7d879f1a665f2d2e33e14e84f04a7452
Mark Spanbroek <mark@spanbroek.net>
2019-08-18 20:02:58 +0200

    Remove unnecessary singleParty

--- tests/NiMPC/SecretSharing/testSecretInternals.nim
+++ tests/NiMPC/SecretSharing/testSecretInternals.nim
@@ -9,9 +9,8 @@ import NiMPC/SecretSharing/Internals
 suite "secret sharing internals":
 
   asynctest "opens a secret without losing precision":
-    singleParty:
-      let large = high(uint32)
-      let secret = Party().rawShare(large)
-      let product = await (secret * 1000'u32).openSumOfShares()
-      check product == 1000'u64 * uint64(large)
+    let large = high(uint32)
+    let secret = Party().rawShare(large)
+    let product = await (secret * 1000'u32).openSumOfShares()
+    check product == 1000'u64 * uint64(large)
 
