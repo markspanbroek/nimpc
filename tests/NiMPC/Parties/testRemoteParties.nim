cb1e573168ac89f7c8a14f3281a448a8824e34c6
Mark Spanbroek <mark@spanbroek.net>
2019-10-20 11:25:39 +0200

    Cleanup test

--- tests/NiMPC/Parties/testRemoteParties.nim
+++ tests/NiMPC/Parties/testRemoteParties.nim
@@ -4,44 +4,40 @@ import http
 import NiMPC/Parties/Basics
 import NiMPC/Parties/Remote
 
-let host = "localhost"
-let port = Port(34590)
-
 suite "remote parties":
 
-  test "they exist":
-    check RemoteParty() != nil
+  let host = "localhost"
+  let port = Port(34590)
 
-  test "they forward messages over a socket":
+  var party: RemoteParty
+
+  setup:
+    party = RemoteParty()
+
+  test "forward messages over a socket":
     proc receiving {.async.} =
       check (await receive(host, port)) == "onetwo"
 
     proc sending {.async.} =
-
-      let party = RemoteParty()
       await party.connect(host, port)
       defer: party.disconnect()
-
       await party.acceptDelivery(Party(), "one")
       await party.acceptDelivery(Party(), "two")
 
     waitFor all(receiving(), sending())
-
-  test "cannot disconnect when not connected":
-    expect Exception:
-      RemoteParty().disconnect()
 
   test "cannot connect twice":
     proc receiving {.async.} =
       discard await receive(host, port)
 
     proc sending {.async.} =
-
-      let party = RemoteParty()
       await party.connect(host, port)
       defer: party.disconnect()
-
       expect Exception:
         await party.connect(host, port)
 
     waitFor all(receiving(), sending())
+
+  test "cannot disconnect when not connected":
+    expect Exception:
+      RemoteParty().disconnect()
