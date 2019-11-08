7a85459cbd560feebd26e7f90f1904b0b6724489
Mark Spanbroek <mark@spanbroek.net>
2019-11-08 14:22:07 +0100

    Ensure that connect() can be called on different types of parties, and on sequences of parties

--- src/NiMPC/Parties/Connections.nim
+++ src/NiMPC/Parties/Connections.nim
@@ -1,11 +1,17 @@
 import Basics
 import sequtils
 
-proc connect*[T: Party](parties: varargs[T]) =
+proc connectImpl(parties: varargs[Party]) =
   for party1 in parties:
     for party2 in parties:
       if party1 != party2:
         party1.peers.add(party2)
+
+proc connect*(parties: varargs[Party]) =
+  connectImpl(parties)
+
+proc connect*[T: Party](parties: varargs[T]) =
+  connectImpl(parties.mapIt(Party(it)))
 
 proc isFirst*[T: Party](party: T): bool =
   party.peers.allIt(party < it)
--- tests/NiMPC/Parties/testPartyConnections.nim
+++ tests/NiMPC/Parties/testPartyConnections.nim
@@ -1,7 +1,9 @@
 import unittest
 import sequtils
 import math
+import examples/Identity
 import NiMPC/Parties/Local
+import NiMPC/Parties/Remote
 import NiMPC/Parties/Connections
 
 suite "Party connections":
@@ -12,6 +14,14 @@ suite "Party connections":
     check party1.peers == @[Party(party2), Party(party3)]
     check party2.peers == @[Party(party1), Party(party3)]
     check party3.peers == @[Party(party1), Party(party2)]
+
+  test "connects different types of parties":
+    let local = newLocalParty()
+    let remote = newRemoteParty(exampleIdentity())
+    connect(local, remote)
+
+  test "connects sequence of parties":
+    connect newSeqWith(3, newLocalParty())
 
   test "only one party is the first party":
     let parties = newSeqWith(3, newLocalParty())
